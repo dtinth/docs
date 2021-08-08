5c3a480b4508055ae9645cc11c01295dcc55b007
dtinth on MBP M1 <dtinth@spacet.me>
2021-08-08 16:52:11 +0700

    Source documentation list from Google Sheets

--- src/sources.mjs
+++ src/sources.mjs
@@ -1,5 +1,6 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import axios from 'axios'
 
 const packageDir = dirname(dirname(fileURLToPath(import.meta.url)))
 
@@ -14,14 +15,29 @@ export const sources = [
     branches: 'HEAD',
     start_path: 'docs',
   },
-  {
-    url: 'https://github.com/dtinth/dtinth.tools-android.git',
-    branches: 'main',
-    start_path: 'docs',
-  },
-  {
-    url: 'https://github.com/dtinth/encrypted.git',
-    branches: 'main',
-    start_path: 'docs',
-  },
 ]
+
+async function fetchDocumentationProjects() {
+  const baseUrl = 'https://sheet.spacet.me'
+  const sheetId = '1kFc9bDizrANULuVSUuHYiZENdPzXZpXvuUWlbZbF_uU'
+  const sheetName = 'Docs'
+  const endpoint = `${baseUrl}/${sheetId}/${sheetName}.json`
+  const {
+    data: { values },
+  } = await axios.get(endpoint)
+  const [header, ...rows] = values
+  return rows.map((row) => {
+    const entries = header.map((key, i) => [key, row[i]])
+    return Object.fromEntries(entries)
+  })
+}
+
+for (const p of await fetchDocumentationProjects()) {
+  if (p.enabled === 'TRUE') {
+    sources.push({
+      url: p.url,
+      branches: p.branches,
+      start_path: 'docs',
+    })
+  }
+}
